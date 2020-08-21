be4e9c98f52fb608a534b88961957cae20a50fd5
GerardoQ <gerardoquispe65@gmail.com>
2020-08-21 01:07:25 -0500

    Updates

---
+++ precache-manifest.4d9d7eeba9a4ea5892ef48775647dfb1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da1703b1f700f4c0b3b356a9d327fd5c",
+    "url": "/ServiYa/index.html"
+  },
+  {
+    "revision": "7c69d61bb7b0ab6a50c1",
+    "url": "/ServiYa/static/css/main.709e93ce.chunk.css"
+  },
+  {
+    "revision": "87b3061a21abb9f8c30a",
+    "url": "/ServiYa/static/js/2.7d7d29ac.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ServiYa/static/js/2.7d7d29ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c69d61bb7b0ab6a50c1",
+    "url": "/ServiYa/static/js/main.efab994e.chunk.js"
+  },
+  {
+    "revision": "28943224f6c9344f73b6",
+    "url": "/ServiYa/static/js/runtime-main.77a59eda.js"
+  },
+  {
+    "revision": "1a440430dc31c3326b9d23cda5fb14fc",
+    "url": "/ServiYa/static/media/CocoGothic_trial.1a440430.ttf"
+  },
+  {
+    "revision": "34dcf3a0a20ef8372295d74bba2367c5",
+    "url": "/ServiYa/static/media/baker.34dcf3a0.svg"
+  },
+  {
+    "revision": "24e0ccf0d5fba5f79eeafa4be6171d96",
+    "url": "/ServiYa/static/media/carpenter.24e0ccf0.svg"
+  },
+  {
+    "revision": "5cbad09b518ae92cb5c2271d102ce56f",
+    "url": "/ServiYa/static/media/cuadrito.5cbad09b.svg"
+  },
+  {
+    "revision": "fcac64b744bc614c3d22c5cb8011b553",
+    "url": "/ServiYa/static/media/electrician.fcac64b7.svg"
+  },
+  {
+    "revision": "bf44612847484289c9133fdff15b36f9",
+    "url": "/ServiYa/static/media/electricista.bf446128.jpg"
+  },
+  {
+    "revision": "0ae0725e9f1abfc046cb3c5601d51c42",
+    "url": "/ServiYa/static/media/logoicon.0ae0725e.svg"
+  },
+  {
+    "revision": "b99a10befc3cf2503ec488500e04a625",
+    "url": "/ServiYa/static/media/loguito.b99a10be.svg"
+  },
+  {
+    "revision": "653be9a186f59ef368eaf1e0b0ff054d",
+    "url": "/ServiYa/static/media/mechanic.653be9a1.svg"
+  },
+  {
+    "revision": "d8817d7dced0e88e32a8447318ab8d12",
+    "url": "/ServiYa/static/media/servicarpintero.d8817d7d.jpg"
+  },
+  {
+    "revision": "c617801b976b16fb2019050b6580fabe",
+    "url": "/ServiYa/static/media/servimecanico.c617801b.jpg"
+  }
+]);
